edc2f3443dfdb0fff80b01a6ce7683e2ad262771
Eric Henderson <henderea@gmail.com>
2019-06-12 14:27:00 -0400

    start working on config db stuff

--- bin/gdrive.js
+++ bin/gdrive.js
@@ -1,1 +1,31 @@
+const _ = require('lodash');
 const db = require('../lib/db');
+
+
+const run = async () => {
+    await db.init();
+
+    let configs = await db.config.getAll();
+
+    console.log('Configs:', JSON.stringify(configs, null, 4));
+
+    await db.config.unset('time.now');
+
+    configs = await db.config.getAll();
+
+    console.log('Configs:', JSON.stringify(configs, null, 4));
+
+    await db.config.set('time.now', String(_.now()));
+
+    configs = await db.config.getAll();
+
+    console.log('Configs:', JSON.stringify(configs, null, 4));
+
+    let time_now = await db.config.get('time.now');
+
+    console.log("Config 'time.now':", time_now);
+
+    await db.close();
+}
+
+run();
